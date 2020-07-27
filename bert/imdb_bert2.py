b91a93cad7636abaa280b167f824e07e41a658e2
kimsoowan <herosdoor@paran.com>
2020-07-27 22:13:25 +0900

    working imdb bert

---
+++ bert/imdb_bert2.py
@@ -0,0 +1,197 @@
+import pandas as pd
+import numpy as np
+import bert
+import tensorflow as tf
+import tensorflow_hub as hub
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
+from tqdm import tqdm
+import re
+import os
+from tokenizers import BertWordPieceTokenizer
+from transformers import BertTokenizer, TFBertModel, BertConfig
+
+
+MAX_SEQ_LEN = 500  # max sequence length
+
+
+def get_masks(tokens):
+    """Masks: 1 for real tokens and 0 for paddings"""
+    return [1] * len(tokens) + [0] * (MAX_SEQ_LEN - len(tokens))
+
+
+def get_segments(tokens):
+    """Segments: 0 for the first sequence, 1 for the second"""
+    segments = []
+    current_segment_id = 0
+    for token in tokens:
+        segments.append(current_segment_id)
+        if token == "[SEP]":
+            current_segment_id = 1
+    return segments + [0] * (MAX_SEQ_LEN - len(tokens))
+
+
+def get_ids(tokens, ids):
+    """Token ids from Tokenizer vocab"""
+    token_ids = ids
+    input_ids = token_ids + [0] * (MAX_SEQ_LEN - len(token_ids))
+    return input_ids
+
+
+def create_single_input(sentence, tokenizer, max_len):
+    """Create an input from a sentence"""
+    # stokens = tokenizer.tokenize(sentence)
+    # stokens = stokens[:max_len]
+    # stokens = ["[CLS]"] + stokens + ["[SEP]"]
+
+    encoded = tokenizer.encode(sentence)
+
+    ids = get_ids(encoded.tokens, encoded.ids)
+    masks = get_masks(encoded.tokens)
+    segments = get_segments(encoded.tokens)
+
+    return ids, masks, segments
+
+
+def convert_sentences_to_features(sentences, tokenizer):
+    """Convert sentences to features: input_ids, input_masks and input_segments"""
+    input_ids, input_masks, input_segments = [], [], []
+
+    for sentence in tqdm(sentences, position=0, leave=True):
+        ids, masks, segments = create_single_input(sentence, tokenizer, MAX_SEQ_LEN - 2)
+        assert len(ids) == MAX_SEQ_LEN
+        assert len(masks) == MAX_SEQ_LEN
+        assert len(segments) == MAX_SEQ_LEN
+        input_ids.append(ids)
+        input_masks.append(masks)
+        input_segments.append(segments)
+
+    return [np.asarray(input_ids, dtype=np.int32), np.asarray(input_masks, dtype=np.int32), np.asarray(input_segments, dtype=np.int32)]
+
+
+def create_tonkenizer(bert_layer):
+    """Instantiate Tokenizer with vocab"""
+    vocab_file = bert_layer.resolved_object.vocab_file.asset_path.numpy()
+    do_lower_case = bert_layer.resolved_object.do_lower_case.numpy()
+    tokenizer = bert.bert_tokenization.FullTokenizer(vocab_file, do_lower_case)
+    return tokenizer
+
+
+def nlp_model(callable_object):
+    # Load the pre-trained BERT base model
+    bert_layer = hub.KerasLayer(handle=callable_object, trainable=True)
+
+    # BERT layer three inputs: ids, masks and segments
+    input_ids = Input(shape=(MAX_SEQ_LEN,), dtype=tf.int32, name="input_ids")
+    input_masks = Input(shape=(MAX_SEQ_LEN,), dtype=tf.int32, name="input_masks")
+    input_segments = Input(shape=(MAX_SEQ_LEN,), dtype=tf.int32, name="segment_ids")
+
+    inputs = [input_ids, input_masks, input_segments]  # BERT inputs
+    pooled_output, sequence_output = bert_layer(inputs)  # BERT outputs
+
+    # Add a hidden layer
+    x = Dense(units=768, activation="relu")(pooled_output)
+    x = Dropout(0.1)(x)
+
+    # Add output layer
+    outputs = Dense(1, activation="sigmoid")(x)
+
+    # Construct a new model
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
+
+
+# model = nlp_model("https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/1")
+model = nlp_model("https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/2")
+model.summary()
+
+# https://www.kaggle.com/lakshmi25npathi/imdb-dataset-of-50k-movie-reviews
+movie_reviews = pd.read_csv("bert/IMDB Dataset.csv")
+movie_reviews.head(5)
+movie_reviews = movie_reviews.sample(frac=1)
+
+print(movie_reviews.isnull().values.any())
+
+print(movie_reviews.shape)
+
+
+def preprocess_text(sen):
+    # Removing html tags
+    sentence = remove_tags(sen)
+
+    # Remove punctuations and numbers
+    sentence = re.sub("[^a-zA-Z]", " ", sentence)
+
+    # Single character removal
+    sentence = re.sub(r"\s+[a-zA-Z]\s+", " ", sentence)
+
+    # Removing multiple spaces
+    sentence = re.sub(r"\s+", " ", sentence)
+
+    return sentence
+
+
+TAG_RE = re.compile(r"<[^>]+>")
+
+
+def remove_tags(text):
+    return TAG_RE.sub("", text)
+
+
+reviews = []
+sentences = list(movie_reviews["review"])
+for sen in sentences:
+    reviews.append(preprocess_text(sen))
+
+print(movie_reviews.columns.values)
+
+print(movie_reviews.sentiment.unique())
+
+y = movie_reviews["sentiment"]
+
+y = np.array(list(map(lambda x: 1 if x == "positive" else 0, y)))
+
+masks = tf.one_hot(y, 1)
+
+slow_tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+save_path = "bert_base_uncased/"
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
+slow_tokenizer.save_pretrained(save_path)
+
+# Load the fast tokenizer from saved file
+tokenizer = BertWordPieceTokenizer("bert_base_uncased/vocab.txt", lowercase=True)
+tokenizer.enable_truncation(MAX_SEQ_LEN - 2)
+
+X_train = convert_sentences_to_features(reviews[:40000], tokenizer)
+X_test = convert_sentences_to_features(reviews[40000:], tokenizer)
+
+one_hot_encoded = to_categorical(y)
+y_train = one_hot_encoded[:40000]
+y_test = one_hot_encoded[40000:]
+
+BATCH_SIZE = 8
+EPOCHS = 1
+
+# Use Adam optimizer to minimize the categorical_crossentropy loss
+opt = Adam(learning_rate=2e-5)
+model.compile(optimizer=opt,
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+# Fit the data to the model
+history = model.fit(X_train, y_train,
+                    validation_data=(X_test, y_test),
+                    epochs=EPOCHS,
+                    batch_size=BATCH_SIZE,
+                    verbose = 1)
+
+# Save the trained model
+# model.save('nlp_model.h5')
+
+pred_test = np.argmax(model.predict(X_test), axis=1)
+print(pred_test[:10])
+print(reviews[40000:40010])
---
+++ bert/imdbbert.py
@@ -0,0 +1,282 @@
+
+import os
+import re
+import json
+import string
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tokenizers import BertWordPieceTokenizer
+from transformers import BertTokenizer, TFBertModel, BertConfig
+import pandas as pd
+
+max_len = 384
+configuration = BertConfig()  # default paramters and configuration for BERT
+
+"""
+## Set-up BERT tokenizer
+"""
+# Save the slow pretrained tokenizer
+slow_tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+save_path = "bert_base_uncased/"
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
+slow_tokenizer.save_pretrained(save_path)
+
+# Load the fast tokenizer from saved file
+tokenizer = BertWordPieceTokenizer("bert_base_uncased/vocab.txt", lowercase=True)
+
+movie_reviews = pd.read_csv('bert/IMDB Dataset.csv')
+movie_reviews.head(5)
+
+print(movie_reviews.isnull().values.any())
+
+print(movie_reviews.shape)
+
+def preprocess_text(sen):
+    # Removing html tags
+    sentence = remove_tags(sen)
+
+    # Remove punctuations and numbers
+    sentence = re.sub('[^a-zA-Z]', ' ', sentence)
+
+    # Single character removal
+    sentence = re.sub(r"\s+[a-zA-Z]\s+", ' ', sentence)
+
+    # Removing multiple spaces
+    sentence = re.sub(r'\s+', ' ', sentence)
+
+    return sentence
+
+
+TAG_RE = re.compile(r'<[^>]+>')
+
+
+def remove_tags(text):
+    return TAG_RE.sub('', text)
+
+
+reviews = []
+sentences = list(movie_reviews['review'])
+for sen in sentences:
+    reviews.append(preprocess_text(sen))
+
+print(movie_reviews.columns.values)
+
+print(movie_reviews.sentiment.unique())
+
+y = movie_reviews['sentiment']
+
+y = np.array(list(map(lambda x: 1 if x=="positive" else 0, y)))
+
+print(reviews[1])
+print(y[1])
+
+context = " ".join(str(reviews[1]).split())
+
+tokenized_context = tokenizer.encode(context)
+
+"""
+## Load the data
+"""
+train_data_url = "https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v1.1.json"
+train_path = keras.utils.get_file("train.json", train_data_url)
+eval_data_url = "https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v1.1.json"
+eval_path = keras.utils.get_file("eval.json", eval_data_url)
+
+"""
+## Preprocess the data
+
+1. Go through the JSON file and store every record as a `SquadExample` object.
+2. Go through each `SquadExample` and create `x_train, y_train, x_eval, y_eval`.
+"""
+
+
+class SquadExample:
+    def __init__(self, question, context, start_char_idx, answer_text, all_answers):
+        self.question = question
+        self.context = context
+        self.start_char_idx = start_char_idx
+        self.answer_text = answer_text
+        self.all_answers = all_answers
+        self.skip = False
+
+    def preprocess(self):
+        context = self.context
+        question = self.question
+        answer_text = self.answer_text
+        start_char_idx = self.start_char_idx
+
+        # Clean context, answer and question
+        context = " ".join(str(context).split())
+        question = " ".join(str(question).split())
+        answer = " ".join(str(answer_text).split())
+
+        # Find end character index of answer in context
+        end_char_idx = start_char_idx + len(answer)
+        if end_char_idx >= len(context):
+            self.skip = True
+            return
+
+        # Mark the character indexes in context that are in answer
+        is_char_in_ans = [0] * len(context)
+        for idx in range(start_char_idx, end_char_idx):
+            is_char_in_ans[idx] = 1
+
+        # Tokenize context
+        tokenized_context = tokenizer.encode(context)
+
+        # Find tokens that were created from answer characters
+        ans_token_idx = []
+        for idx, (start, end) in enumerate(tokenized_context.offsets):
+            if sum(is_char_in_ans[start:end]) > 0:
+                ans_token_idx.append(idx)
+
+        if len(ans_token_idx) == 0:
+            self.skip = True
+            return
+
+        # Find start and end token index for tokens from answer
+        start_token_idx = ans_token_idx[0]
+        end_token_idx = ans_token_idx[-1]
+
+        # Tokenize question
+        tokenized_question = tokenizer.encode(question)
+
+        # Create inputs
+        input_ids = tokenized_context.ids + tokenized_question.ids[1:]
+        token_type_ids = [0] * len(tokenized_context.ids) + [1] * len(
+            tokenized_question.ids[1:]
+        )
+        attention_mask = [1] * len(input_ids)
+
+        # Pad and create attention masks.
+        # Skip if truncation is needed
+        padding_length = max_len - len(input_ids)
+        if padding_length > 0:  # pad
+            input_ids = input_ids + ([0] * padding_length)
+            attention_mask = attention_mask + ([0] * padding_length)
+            token_type_ids = token_type_ids + ([0] * padding_length)
+        elif padding_length < 0:  # skip
+            self.skip = True
+            return
+
+        self.input_ids = input_ids
+        self.token_type_ids = token_type_ids
+        self.attention_mask = attention_mask
+        self.start_token_idx = start_token_idx
+        self.end_token_idx = end_token_idx
+        self.context_token_to_char = tokenized_context.offsets
+
+
+with open(train_path) as f:
+    raw_train_data = json.load(f)
+
+with open(eval_path) as f:
+    raw_eval_data = json.load(f)
+
+
+def create_squad_examples(raw_data):
+    squad_examples = []
+    for item in raw_data["data"]:
+        for para in item["paragraphs"]:
+            context = para["context"]
+            for qa in para["qas"]:
+                question = qa["question"]
+                answer_text = qa["answers"][0]["text"]
+                all_answers = [_["text"] for _ in qa["answers"]]
+                start_char_idx = qa["answers"][0]["answer_start"]
+                squad_eg = SquadExample(
+                    question, context, start_char_idx, answer_text, all_answers
+                )
+                squad_eg.preprocess()
+                squad_examples.append(squad_eg)
+    return squad_examples
+
+
+def create_inputs_targets(squad_examples):
+    dataset_dict = {
+        "input_ids": [],
+        "token_type_ids": [],
+        "attention_mask": [],
+        "start_token_idx": [],
+        "end_token_idx": [],
+    }
+    for item in squad_examples:
+        if item.skip == False:
+            for key in dataset_dict:
+                dataset_dict[key].append(getattr(item, key))
+    for key in dataset_dict:
+        dataset_dict[key] = np.array(dataset_dict[key])
+
+    x = [
+        dataset_dict["input_ids"],
+        dataset_dict["token_type_ids"],
+        dataset_dict["attention_mask"],
+    ]
+    y = [dataset_dict["start_token_idx"], dataset_dict["end_token_idx"]]
+    return x, y
+
+
+train_squad_examples = create_squad_examples(raw_train_data)
+x_train, y_train = create_inputs_targets(train_squad_examples)
+print(f"{len(train_squad_examples)} training points created.")
+
+eval_squad_examples = create_squad_examples(raw_eval_data)
+x_eval, y_eval = create_inputs_targets(eval_squad_examples)
+print(f"{len(eval_squad_examples)} evaluation points created.")
+
+"""
+Create the Question-Answering Model using BERT and Functional API
+"""
+
+def create_model():
+    encoder = TFBertModel.from_pretrained("bert-base-uncased")
+
+    input_ids = layers.Input(shape=(max_len,), dtype=tf.int32)
+    token_type_ids = layers.Input(shape=(max_len,), dtype=tf.int32)
+    attention_mask = layers.Input(shape=(max_len,), dtype=tf.int32)
+    embedding = encoder(
+        input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask
+    )[0]
+
+    start_logits = layers.Dense(1, use_bias=False)(embedding)
+    start_logits = layers.Flatten()(start_logits)
+
+    start_probs = layers.Activation(keras.activations.softmax)(start_logits)
+
+    model = keras.Model(
+        inputs=[input_ids, token_type_ids, attention_mask],
+        outputs=start_probs,
+    )
+    optimizer = keras.optimizers.Adam(lr=5e-5)
+    model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+
+model = create_model()
+
+model.summary()
+
+"""
+## Create evaluation Callback
+
+This callback will compute the exact match score using the validation data
+after every epoch.
+"""
+
+"""
+## Train and Evaluate
+"""
+model.fit(
+    x_train,
+    y_train,
+    validation_data=(x_eval, y_eval),
+    epochs=1,  # For demonstration, 3 epochs are recommended
+    verbose=2,
+    batch_size=64,
+    # callbacks=[exact_match_callback],
+)
+
+model.save('nlp_model.h5')
